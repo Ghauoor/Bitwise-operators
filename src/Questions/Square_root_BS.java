9635bea708dcf65a2d2ecfa8cc405526513bbd43
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-09-23 03:12:29 +0500

    squarerootnum Using BS

---
+++ src/Questions/Square_root_BS.java
@@ -0,0 +1,38 @@
+package Questions;
+
+public class Square_root_BS {
+    public static void main(String[] args) {
+        int n = 40;
+        int p = 3;
+
+        System.out.println(sqrt(n, p));
+    }
+    static double sqrt(int n, int p){
+        int s = 0;
+        int e = n;
+        double root = 0.0;
+
+        while (s <= e){
+            int m = s + (e - s) / 2;
+
+            if (m * m == n){
+                root = m;
+                return m;
+            } else if(m * m > n){
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+        double increment = 0.1;
+
+        for (int i = 0; i < p; i++) {
+            while (root * root <= n){
+                root += increment;
+            }
+            root -= increment;
+            increment /= 10;
+        }
+         return root;
+    }
+}
